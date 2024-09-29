26d25dadcb57a95b84eb5c677343aeb7ae24d71c
guiisbarbosa <gui.silva93@hotmail.com>
2024-09-29 15:01:01 -0300

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import GlobalStyle from './GlobalStyle.tsx'
+import logo from './assets/animais-logo.svg'
+
+export function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <h1>Hello World</h1>
+    </>
+  )
+}
